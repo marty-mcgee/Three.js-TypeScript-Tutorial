bf7405866138bbdbc09ffa9c68b8424249498a63
Marty McGee <support@companyjuice.com>
2022-08-19 09:49:14 -0700

    v0.1.4 Setting up the Webpack Dev Server

--- src/client/webpack.dev.js
+++ src/client/webpack.dev.js
@@ -3,12 +3,12 @@ const common = require('./webpack.common.js')
 const path = require('path');
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'eval-source-map',
-    devServer: {
-        static: {
-            directory: path.join(__dirname, '../../dist/client'),
-        },
-        hot: true,
+  mode: 'development',
+  devtool: 'eval-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '../../dist/client'),
     },
+    hot: true,
+  },
 })
